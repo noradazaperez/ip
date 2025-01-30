4247c3f6d82e1fe6c16c2b2851dcb955541d1ac3
noradazaperez <65602872+ndaza03@users.noreply.github.com>
2025-01-30 21:11:20 +0800

    Level-3

--- src/main/java/Mimi.java
+++ src/main/java/Mimi.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Mimi {
     static String bar = "--------------------------------------------------";
+    static Task [] tasks = new Task[100];
+
+    static int taskIndex = 0;
 
     public static void main(String[] args) {
         String userinput;
@@ -9,11 +12,45 @@ public class Mimi {
         Scanner sc = new Scanner(System.in);
         do {
             userinput = sc.nextLine();
-            System.out.println(bar + "\n" + userinput + "\n" + bar);
+            System.out.println(bar);
+            String [] input = userinput.split(" ");
+            switch (input[0]) {
+                case "list":
+                    showList();
+                    break;
+                case "mark":
+                    mark(Integer.parseInt(input[1]));
+                    break;
+                case "unmark":
+                    unmark(Integer.parseInt(input[1]));
+                    break;
+                default:
+                    addTask(userinput);
+            }
+
+            System.out.println(bar);
 
         } while (!userinput.equals("bye"));
         System.out.println("Bye. Hope to see you again soon.\n" + bar);
 
+    }
+
+    public static void mark(int index){
+        if (index - 1 > taskIndex) {
+            System.out.println("Sorry! The task doesn't exist.");
+        }
+        tasks[index - 1].markAsDone();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(tasks[index - 1]);
+    }
+
+    public static void unmark(int index){
+        if (index - 1 > taskIndex) {
+            System.out.println("Sorry! The task doesn't exist.");
+        }
+        tasks[index - 1].markAsNotDone();
+        System.out.println("OK, I've marked this task as not done yet: ");
+        System.out.println(tasks[index - 1]);
     }
 
     public static void greet() {
@@ -28,5 +65,19 @@ public class Mimi {
         System.out.println("Hello! I'm Mimi");
         System.out.println("What can I do for you?\n" + bar);
 
+    }
+
+    public static void addTask(String taskDescription) {
+        Task task = new Task(taskDescription);
+        tasks[taskIndex] = task;
+        taskIndex++;
+
+        System.out.println("added: " + task.getDescription());
+    }
+
+    public static void showList() {
+        for (int i = 0; i < taskIndex; i++) {
+            System.out.println((i+1) + ". " + tasks[i].toString());
+        }
     }
 }
