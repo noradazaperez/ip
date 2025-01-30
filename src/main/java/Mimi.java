7ae47513cf2f887259ec970dab8b8e97403deb9f
noradazaperez <65602872+ndaza03@users.noreply.github.com>
2025-01-30 19:46:39 +0800

    Level-0

---
+++ src/main/java/Mimi.java
@@ -0,0 +1,17 @@
+public class Mimi {
+    public static void main(String[] args) {
+        String logo = """
+                 M   M  III  M   M  III\s
+                 MM MM   I   MM MM   I\s
+                 M M M   I   M M M   I\s
+                 M   M   I   M   M   I\s
+                 M   M  III  M   M  III\s
+                """;
+        String bar = "--------------------------------------------------";
+
+        System.out.println(bar + "\nHello from\n" + logo + bar);
+        System.out.println("Hello! I'm Mimi");
+        System.out.println("What can I do for you?\n" + bar);
+        System.out.println("Bye. Hope to see you again soon.\n" + bar);
+    }
+}
